bd99aef6ff5d06de054b2e9bfe83a80680b4063c
roacv <roacv@hotmail.com>
2023-01-20 22:24:09 +0000

    Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoImageUrl from "../../img/logo.png";
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
+		// <nav className="navbar navbar-light bg-light">
+		// 	<div className="container">
+		// 		<Link to="/">
+		// 			<img className="d-inline-block  me-2" src={rigoImageUrl} alt="Logo" width="100" height="50" />
+		// 			<span className="navbar-brand ms-2 mb-0 h1">Inicio</span>
+		// 		</Link>
+		// 		<div className="ml-auto">
+		// 			<Link to="/demo">
+		// 				<button className="btn btn-primary">Check the Context in action</button>
+		// 			</Link>
+		// 		</div>   
+		// 	</div>
+		// </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <Link className="nav-brand" to="/">
+                    <img class="d-inline-block " src={logoImageUrl} alt="Logo" width="120" height="50" />
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav ms-auto me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Inicio</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/shop">Tienda</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/contact">Contacto</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="buscar" aria-label="search"/>
+                        <button className="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+            </nav>
 	);
 };
