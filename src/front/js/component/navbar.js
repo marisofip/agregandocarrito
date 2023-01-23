9225e27ccfdf378d0121fafb3ce6d12bb828451a
marisofip <marisofipalencia@gmail.com>
2023-01-23 13:38:42 +0000

    agregando shop y login

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoImageUrl from "../../img/logo.png";
+import { Tienda } from "../pages/shop";
 
 
 export const Navbar = () => {
