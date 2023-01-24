c374b99eeefe8b032348701fc82ab7d5921325ab
luisrcc <luiscastillo1991@gmail.com>
2023-01-24 13:45:04 +0000

    agregando cambios a categorias

--- src/front/js/pages/create_category.js
+++ src/front/js/pages/create_category.js
@@ -12,55 +12,128 @@ export const Categoria = () => {
                 <div className='row'>
                     <div className='col'>
                         <table className="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">NOMBRE </th>
+                                    <th scope="col">TIPO</th>
+                                    {/* <th scope="col">Handle</th> */}
+                                </tr>
+                            </thead>
                             <tbody>
                                 <tr>
-                                    <td>
-                                        Categoria 1{" "}
+                                    <th scope="row">1</th>
+                                    <td>Tecnologia
                                         <button className="border border-0 bg-transparent float-md-end">
-                                            {" "}
-                                            <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
                                         </button>
                                     </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td> */}
+
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Categoria 2{" "}
+                                    <th scope="row">2</th>
+                                    <td>Tecnologia
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
                                         <button className="border border-0 bg-transparent float-md-end">
                                             <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
                                         </button>
-                                    </td>
+                                    </td> */}
+
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Categoria 3{" "}
+                                    <th scope="row">3</th>
+                                    <td>Tecnologia
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
                                         <button className="border border-0 bg-transparent float-md-end">
                                             <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
                                         </button>
-                                    </td>
+                                    </td> */}
+
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Categoria 4{" "}
+                                    <th scope="row">4</th>
+                                    <td>Tecnologia
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
                                         <button className="border border-0 bg-transparent float-md-end">
                                             <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
                                         </button>
-                                    </td>
+                                    </td> */}
+
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Categoria 5{" "}
+                                    <th scope="row">5</th>
+                                    <td>Tecnologia
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
                                         <button className="border border-0 bg-transparent float-md-end">
                                             <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
                                         </button>
-                                    </td>
+                                    </td> */}
+
                                 </tr>
                                 <tr>
-                                    <td>
-                                        Categoria 6{" "}
+                                    <th scope="row">6</th>
+                                    <td>Tecnologia
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    <td>Electronica
+                                        <button className="border border-0 bg-transparent float-md-end">
+                                            <img src="https://img.icons8.com/material-sharp/1x/pencil--v2.png" />{" "}
+                                        </button>
+                                    </td>
+                                    {/* <td>@mdo
                                         <button className="border border-0 bg-transparent float-md-end">
                                             <img src="https://img.icons8.com/material-sharp/2x/pencil--v2.png" />{" "}
                                         </button>
-                                    </td>
+                                    </td> */}
+
                                 </tr>
                             </tbody>
                         </table>
