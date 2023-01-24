b4afececa51f858d45f4ac6e440e51016f8c6393
marisofip <marisofipalencia@gmail.com>
2023-01-24 15:51:00 +0000

    reutilizando card

--- src/front/js/pages/shop.js
+++ src/front/js/pages/shop.js
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { Navbar } from "react-bootstrap";
+import { Card } from "../component/cards";
+import { Detail } from "../component/detail-prod";
 import { Context } from "../store/appContext";
 
 export const Shop = () => {
@@ -9,210 +11,22 @@ export const Shop = () => {
         {" "}
         CATEGORIAS
       </h1>
-      <div className="container  justify-content-center mt-5  mb-5">
-        <div className="row row-cols-1 row-cols-md-3 mb-3 text-center">
-          <div className="col">
-            <div className="card mb-4 rounded-3">
-              <div className="card-header py-3">
-                <svg
-                  className="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595C"></rect>
-                  <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                    Imagen Producto
-                  </text>
-                </svg>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">Categoria 1</h1>
-
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-outline-dark"
-                >
-                  Revisar productos
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-4 rounded-3 shadow-sm">
-              <div className="card-header py-3">
-                <svg
-                  className="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595C"></rect>
-                  <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                    Imagen Producto
-                  </text>
-                </svg>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">Categoria 2</h1>
-
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-outline-dark"
-                >
-                  Revisar productos
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-4 rounded-3 ">
-              <div className="card-header py-3 ">
-                <svg
-                  className="bd-placeholder-img card-img-top"
-                  width="100%"
-                  height="400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: Thumbnail"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="#55595C"></rect>
-                  <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                    Imagen Producto
-                  </text>
-                </svg>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">Categoria 3</h1>
-
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-outline-dark"
-                >
-                  Revisar productos
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="row">
-            <div className="col-sm-4">
-              <div className="card">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <svg
-                      className="bd-placeholder-img card-img-top"
-                      width="100%"
-                      height="150"
-                      xmlns="http://www.w3.org/2000/svg"
-                      role="img"
-                      aria-label="Placeholder: Thumbnail"
-                      preserveAspectRatio="xMidYMid slice"
-                      focusable="false"
-                    >
-                      <title>Placeholder</title>
-                      <rect width="100%" height="100%" fill="#55595C"></rect>
-                      <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                        Imagen{" "}
-                      </text>
-                    </svg>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">Producto 1</h5>
-                      <p className="card-text">30.000$</p>
-                      <a href="#" className="btn btn-dark">
-                        Ver
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <svg
-                      className="bd-placeholder-img card-img-top"
-                      width="100%"
-                      height="150"
-                      xmlns="http://www.w3.org/2000/svg"
-                      role="img"
-                      aria-label="Placeholder: Thumbnail"
-                      preserveAspectRatio="xMidYMid slice"
-                      focusable="false"
-                    >
-                      <title>Placeholder</title>
-                      <rect width="100%" height="100%" fill="#55595C"></rect>
-                      <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                        Imagen{" "}
-                      </text>
-                    </svg>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">Producto 2</h5>
-                      <p className="card-text">30.000$</p>
-                      <a href="#" className="btn btn-dark">
-                        Ver
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-sm-4">
-              <div className="card">
-                <div className="row g-0">
-                  <div className="col-md-4">
-                    <svg
-                      className="bd-placeholder-img card-img-top"
-                      width="100%"
-                      height="150"
-                      xmlns="http://www.w3.org/2000/svg"
-                      role="img"
-                      aria-label="Placeholder: Thumbnail"
-                      preserveAspectRatio="xMidYMid slice"
-                      focusable="false"
-                    >
-                      <title>Placeholder</title>
-                      <rect width="100%" height="100%" fill="#55595C"></rect>
-                      <text x="20%" y="50%" fill="#ECEEEF" dy=".3em">
-                        Imagen{" "}
-                      </text>
-                    </svg>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">Producto 3</h5>
-                      <p className="card-text">30.000$</p>
-                      <a href="#" className="btn btn-dark">
-                        Ver
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="container d-flex justify-content-center mt-5  mb-5">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div className="container d-flex justify-content-center ">
+        <Detail />
+        <Detail />
+        <Detail />
+        <Detail />
+      </div>
+      <div className="container d-flex justify-content-center">
+        <Detail />
+        <Detail />
+        <Detail />
+        <Detail />
       </div>
     </>
   );
